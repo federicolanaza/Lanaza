f31ca10b022fa33de8454389b017bb06fd2459e6
Federico S. Lanaza <federico.lanaza@neu.edu.ph>
2026-03-19 09:56:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5352573047-8ccbc",
+  "appId": "1:666098089730:web:b9bf4a08303121128a4c1b",
+  "apiKey": "AIzaSyAP2adDVAemozM36SrgS5JB5JjtOOU3nSQ",
+  "authDomain": "studio-5352573047-8ccbc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "666098089730"
+};
